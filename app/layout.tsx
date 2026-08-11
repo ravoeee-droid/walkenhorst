e7b82eebc5a336fb1efdc03c398715787538c20a
ravoeee-droid <ravoeee@gmail.com>
2026-08-11 16:00:17 +0200

    style: load prospect website preview animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./outbound-ui.css";
 import "./sales-os.css";
 import "./audit-print.css";
+import "./video-preview.css";
 
 export const metadata: Metadata = {
   title: "Walkenhorst Energy Sales OS",
