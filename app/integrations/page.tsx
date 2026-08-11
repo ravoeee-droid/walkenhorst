223d2ce0d9866d35da117e2ec4ee22ffa752558a
ravoeee-droid <ravoeee@gmail.com>
2026-08-11 17:20:06 +0200

    feat: add chatwoot lead sync to integration center

--- app/integrations/page.tsx
+++ app/integrations/page.tsx
@@ -2,7 +2,8 @@
 
 import { AuthGate } from "@/components/auth-gate";
 import { AdvancedIntegrations } from "@/components/advanced-integrations";
+import { ChatwootLeadSync } from "@/components/chatwoot-lead-sync";
 
 export default function IntegrationsPage(){
-  return <AuthGate>{user=><AdvancedIntegrations user={user}/>}</AuthGate>;
+  return <AuthGate>{user=><><AdvancedIntegrations user={user}/><div style={{maxWidth:1480,margin:"-36px auto 80px",padding:"0 26px"}}><ChatwootLeadSync user={user}/></div></>}</AuthGate>;
 }
