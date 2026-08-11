55fb0f3b1626da94f0ca3b82d720cfcd82c85f48
ravoeee-droid <ravoeee@gmail.com>
2026-08-11 15:46:56 +0200

    feat: expose audit PDF export

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { AuthGate } from "@/components/auth-gate";
+import { AuditPdfButton } from "@/components/audit-pdf-button";
 import { SalesOSDashboard } from "@/components/sales-os-dashboard";
 
 export function AppShell() {
-  return <AuthGate>{(user) => <SalesOSDashboard user={user} />}</AuthGate>;
+  return <AuthGate>{(user) => <><SalesOSDashboard user={user} /><AuditPdfButton /></>}</AuthGate>;
 }
