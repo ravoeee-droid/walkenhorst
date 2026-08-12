56180d1bfb6d01d8985b3ab55a8d130efef8f1d9
ravoeee-droid <ravoeee@gmail.com>
2026-08-12 06:50:04 +0200

    feat: schedule Rinkel reconciliation worker

---
+++ supabase/migrations/033_rinkel_reconciliation_cron.sql
@@ -0,0 +1,26 @@
+create extension if not exists pg_cron;
+create extension if not exists pg_net;
+create extension if not exists supabase_vault;
+
+do $$
+declare existing_job bigint;
+begin
+  select jobid into existing_job from cron.job where jobname='walkenhorst-rinkel-reconcile' limit 1;
+  if existing_job is not null then perform cron.unschedule(existing_job); end if;
+end $$;
+
+select cron.schedule(
+  'walkenhorst-rinkel-reconcile',
+  '*/15 * * * *',
+  $job$
+  select net.http_post(
+    url := 'https://jiahshldcusphxtbqxpv.supabase.co/functions/v1/rinkel-reconcile',
+    headers := jsonb_build_object(
+      'Content-Type','application/json',
+      'x-worker-key',(select decrypted_secret from vault.decrypted_secrets where name='energy_worker_key' limit 1)
+    ),
+    body := jsonb_build_object('limit',20),
+    timeout_milliseconds := 30000
+  );
+  $job$
+);
