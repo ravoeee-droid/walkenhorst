024c61eb9341e8566965295641f4e8f6ad8372ed
ravoeee-droid <ravoeee@gmail.com>
2026-08-13 10:47:27 +0200

    Remove floating module navigation from dashboard

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -2,8 +2,7 @@
 
 import { AuthGate } from "@/components/auth-gate";
 import { SalesOsDashboardV2 } from "@/components/sales-os-dashboard-v2";
-import { SystemModuleLinks } from "@/components/system-module-links";
 
 export function AppShell() {
-  return <AuthGate>{(user) => <><SalesOsDashboardV2 user={user} /><SystemModuleLinks /></>}</AuthGate>;
+  return <AuthGate>{(user) => <SalesOsDashboardV2 user={user} />}</AuthGate>;
 }
