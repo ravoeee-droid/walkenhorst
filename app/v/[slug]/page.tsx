7b0a05c39bca21cbe7ca37d54396d177706780b5
ravoeee-droid <ravoeee@gmail.com>
2026-09-01 05:51:20 +0200

    feat(tracking): attach engagement tracker to public video pages

--- app/v/[slug]/page.tsx
+++ app/v/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { VideoLanding } from "@/components/video-landing";
+import { VideoEngagementTracker } from "@/components/video-engagement-tracker";
 
 export default async function VideoPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  return <VideoLanding slug={slug} />;
+  return <><VideoEngagementTracker slug={slug}/><VideoLanding slug={slug}/></>;
 }
