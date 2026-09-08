dc60db863a093f5a2cdabcf45e1635fed3ac43a0
ravoeee-droid <ravoeee@gmail.com>
2026-09-08 11:18:27 +0200

    Enable realtime updates for caller queue

---
+++ supabase/migrations/20260908091500_enable_caller_queue_realtime.sql
@@ -0,0 +1,3 @@
+alter publication supabase_realtime add table public.energy_followups;
+alter publication supabase_realtime add table public.energy_leads;
+alter publication supabase_realtime add table public.energy_intent_events;
