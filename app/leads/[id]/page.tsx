a88deb12be00dc5510e6bae7747829b18db8cbb6
ravoeee-droid <ravoeee@gmail.com>
2026-09-08 10:23:49 +0200

    feat: show full email history on lead pages

--- app/leads/[id]/page.tsx
+++ app/leads/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { LeadDetailWorkspace } from "@/components/lead-detail-workspace";
+import { LeadEmailHistory } from "@/components/lead-email-history";
 
 export default function LeadDetailPage(){
-  return <LeadDetailWorkspace/>;
+  return <>
+    <LeadDetailWorkspace/>
+    <LeadEmailHistory/>
+  </>;
 }
